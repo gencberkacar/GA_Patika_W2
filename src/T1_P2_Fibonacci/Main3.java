7496e7134af115fed4a2404eb2fd16a445ad9f4c
gencberkacar <154082410+gencberkacar@users.noreply.github.com>
2024-02-29 09:03:10 +0300

    Patika_Week_2

---
+++ src/T1_P2_Fibonacci/Main3.java
@@ -0,0 +1,15 @@
+package T1_P2_Fibonacci;
+
+public class Main3 {
+    static int fib(int n){
+        if (n==1 || n==2){
+            return 1;
+            }
+        return fib(n - 1) + fib(n - 2);
+
+    }
+    public static void main (String[] args){
+        System.out.println(fib(5));
+
+    }
+}
